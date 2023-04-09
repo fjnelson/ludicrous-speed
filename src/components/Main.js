7a23cb3d5a12312b69043139574133e48047e954
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-09 09:04:45 -0700

    adding navbar placements

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,34 +7,34 @@ import UserAccount from './pages/UserAccount';
 import LogIn from './pages/LogIn';
 
 function Main({ currentPage, handlePageChange }) {
-    let renderedPage;
-  
-    switch (currentPage) {
-      case 'Home':
-        renderedPage = <Home />;
-        break;
-      case 'Create':
-        renderedPage = <CreatePost />;
-        break;
-      case 'UserAccount':
-        renderedPage = <UserAccount />;
-        break;
-      case 'LogIn':
-        renderedPage = <LogIn />;
-        break;
-      default:
-        renderedPage = <div>This is not a valid page</div>;
-        break;
-    }
-  
-    return (
-      <div>
-        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-        <Container>
-          {renderedPage}
-        </Container>
-      </div>
-    );
+  let renderedPage;
+
+  switch (currentPage) {
+    case 'Home':
+      renderedPage = <Home />;
+      break;
+    case 'Create':
+      renderedPage = <CreatePost />;
+      break;
+    case 'UserAccount':
+      renderedPage = <UserAccount />;
+      break;
+    case 'LogIn':
+      renderedPage = <LogIn />;
+      break;
+    default:
+      renderedPage = <div>This is not a valid page</div>;
+      break;
   }
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Container fluid>
+        {renderedPage}
+      </Container>
+    </div>
+  );
+}
 
 export default Main;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,21 +3,31 @@ import { Menu } from 'semantic-ui-react';
 
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <Menu inverted>
-      <Menu.Item header>Hello Stranger</Menu.Item>
-      <Menu.Item name='Home' active={currentPage === 'Home'} onClick={() => handlePageChange('Home')} />
-      <Menu.Item name='Create Post' active={currentPage === 'Create'} onClick={() => handlePageChange('Create')} />
-      <Menu.Item name='User Accounts' active={currentPage === 'UserAccount'} onClick={() => handlePageChange('UserAccount')} />
-      <Menu.Menu position='right'>
-        <Menu.Item name='Login' active={currentPage === 'LogIn'} onClick={() => handlePageChange('LogIn')} />
-      </Menu.Menu>
-    </Menu>
+<Menu inverted style={{ display: 'flex', justifyContent: 'space-between' }}>
+  <Menu.Item header>Hello Stranger</Menu.Item>
+  <Menu.Menu id='menu-items'>
+    <Menu.Item
+      name='Home'
+      active={currentPage === 'Home'}
+      onClick={() => handlePageChange('Home')}
+    />
+    <Menu.Item
+      name='Create Post'
+      active={currentPage === 'Create'}
+      onClick={() => handlePageChange('Create')}
+    />
+    <Menu.Item
+      name='User Accounts'
+      active={currentPage === 'UserAccount'}
+      onClick={() => handlePageChange('UserAccount')}
+    />
+  </Menu.Menu>
+  <Menu.Item
+    name='Login'
+    active={currentPage === 'LogIn'}
+    onClick={() => handlePageChange('LogIn')}
+    id='login'
+  />
+</Menu>
   );
 }
-
-
-
-
-
-
-
