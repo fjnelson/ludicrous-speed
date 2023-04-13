4f09953b90b209cc60e1cfc8a18742c35a6f5ed6
Peter Ataras <peterataras@gmail.com>
2023-04-13 15:51:53 -0700

    lost data working on restoring

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,36 +3,46 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type Post {
     _id: ID
-    postText: String!
-    postAuthor: String!
-    createdAt: String!
+    postText: String
+    postAuthor: String
+    createdAt: String
     comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    username: String
+    createdAt: String
   }
   type User {
     _id: ID
     username: String!
     email: String!
     password: String!
-    posts:[Post]
-  }
-
-  type Comment {
-    _id: ID
-    commentText: String!
-    username: String!
-    createdAt: String!
-  }
-
-  type Query {
     posts: [Post]
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+  type Query {
+    posts: [Post]!
     post(postId: ID!): Post
     users: [User]!
     user(userId: ID!): User
   }
 
   type Mutation {
+    addUser(
+      username: String!
+      email: String!
+      password: String!
+    ): Auth
+    login(email: String!, password: String!): Auth
     addPost(postText: String!, postAuthor: String!): Post
-    addComment(postId: ID!, commentText: String!, username: String!): Post
+    addComment(postId: ID!, commentText: String!): Post
     removePost(postId: ID!): Post
     removeComment(postId: ID!, commentId: ID!): Post
   }
