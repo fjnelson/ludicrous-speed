f92cf6d17e63baf42a4d525568b19da111472257
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-13 20:04:57 -0600

    please work

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,17 +1,38 @@
 import React from "react";
-// import { Header } from "semantic-ui-react";
-import HelloStranger from "../images/hellostranger-04.jpg";
+import { Grid, Button } from "semantic-ui-react";
+import hellostranger4 from "../images/hellostranger-04.jpg";
+import "./Home.css";
 
-const HeaderExampleSettingsIcon = () => (
-  <div
-    style={{
-      height: "100vh",
-      backgroundImage: `url(${HelloStranger})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-    }}
-    >
-  </div>
-);
-
-export default HeaderExampleSettingsIcon;
+export default function Home() {
+  return (
+    <body style={{ backgroundColor: "#25495f" }}>
+      //<div style={{ height: "100vh"}}>
+        <div>
+          <img src={hellostranger4} className="picture" />
+          <p style={{ fontSize: "2em", color: "#FFC857", textAlign: "center" }}>
+            “If you see someone without a smile, give them one of yours.” -
+            Dolly Parton - Helen Evans
+          </p>
+        </div>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              <Button></Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button>Hello 2</Button>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Button>Hello 3</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button>Hello 4</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      //</div>
+    //</body>
+  );
+}
