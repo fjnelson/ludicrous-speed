3c5f145c2de2c5de72fa5817c1dca7c935516e07
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-13 21:50:41 -0700

    updating build

--- server/server.js
+++ server/server.js
@@ -18,10 +18,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
