645525c711daac2f8c07954ebd8d950bd93caaad
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-04-14 20:30:13 -0700

    fixing seeds

--- client/src/components/pages/DonateButton.js
+++ client/src/components/pages/DonateButton.js
@@ -8,8 +8,8 @@ function DonateButton() {
     const { error } = await stripe.redirectToCheckout({
       lineItems: [{ price: 'price_1Mv4iDHBDh6WAzZIvNpKAAia', quantity: 1 }],
       mode: 'payment',
-      successUrl: './ThankYou.js',
-      cancelUrl: './ThankYou.js',
+      successUrl: 'https://example.com/success',
+      cancelUrl: 'https://example.com/cancel',
     });
 
     if (error) {
