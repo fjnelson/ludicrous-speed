c35c04065909c1e5c434546a6deef16a7da29d12
cridder <cjridder@gmail.com>
2023-04-15 10:43:14 -0700

    added strip button to fixed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import DonateButton from './pages/DonateButton';
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import DonateButton from "./pages/DonateButton";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 function Footer() {
-  const footerTextStyle = {
-  position: "fixed",
-  bottom: "0",
-  left: "0",
-  width: "100%",
-  padding: "1rem",
+	const footerTextStyle = {
+		position: "fixed",
+		bottom: "0",
+		left: "0",
+		width: "100%",
+		//   padding: "1rem",
+		color: "blue",
+		fontSize: "14px",
+		textAlign: "right",
+		marginTop: "10px",
+	};
 
-    color: "blue",
-    fontSize: "14px",
-    textAlign: "center",
-    marginTop: "10px",
-  };
-
-  return (
+	return (
 		// <footer className="footer-bottom">
-		<footer>
-			<Elements stripe={stripePromise}>
-				<DonateButton />
-			</Elements>
-			<p style={footerTextStyle}>2023 Team Falcon &trade;</p>
-		</footer>
+		<div>
+			<footer style={footerTextStyle}>
+				<Elements stripe={stripePromise}>
+					<DonateButton />
+				</Elements>
+				<p>2023 Team Falcon &trade;</p>
+			</footer>
+		</div>
 	);
 }
 
