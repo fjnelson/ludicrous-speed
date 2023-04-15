3ebcf15f663a7e1b1748b758bbf9fc461b0472b4
cridder <cjridder@gmail.com>
2023-04-15 11:39:45 -0700

    comments and cleanup

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,52 +2,28 @@ import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import DonateButton from "./pages/DonateButton";
+// import semantic cridder 20230415113508
 import {
 	Container,
 	Divider,
 	Grid,
 	Header,
-	Icon,
 	List,
 	Segment,
 } from "semantic-ui-react";
 
-
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
+// function to update year automatically cridder 20230415113513
 function Copyright() {
 	const year = new Date().getFullYear();
 	return <div>&copy; {year} Team Falcon &trade;</div>;
 }
 
 function Footer() {
-	const year = new Date().getFullYear();
-	
-	// const footerTextStyle = {
-	// 	// position: "fixed",
-	// 	backgroundColor: "#255f85",
-	// 	bottom: "0",
-	// 	left: "0",
-	// 	width: "100%",
-	// 	//   padding: "1rem",
-	// 	// color: "blue",
-	// 	fontSize: "14px",
-	// 	textAlign: "right",
-	// 	marginTop: "10px",
-	// };
-
 	return (
-		// <footer className="footer-bottom">
 		<div style={{ backgroundColor: "#255f85", color: "black" }}>
-			{/* <footer style={footerTextStyle}> */}
 			<footer>
-				{/* <Elements stripe={stripePromise}>
-					<DonateButton />
-				</Elements>
-				<p>2023 Team Falcon &trade;</p>
-
-				<Copyright /> */}
-
 				<Segment vertical style={{ padding: "0em 0em" }}>
 					<Container>
 						<Grid divided stackable>
@@ -61,31 +37,15 @@ function Footer() {
 										</List.Item>
 									</List>
 								</Grid.Column>
-								{/* <Grid.Column width={5}> */}
 								<Grid.Column width={5}>
 									<Header as="h4">Donate to fund this site!</Header>
 									<Elements stripe={stripePromise}>
 										<DonateButton />
 									</Elements>
-									{/* <p>
-									Extra space for a call to action inside the footer that could
-									help re-engage users.
-								</p> */}
 								</Grid.Column>
 							</Grid.Row>
 						</Grid>
 						<Divider section />
-						{/* <List horizontal inverted divided link size="small">
-						<List.Item as="a" href="#">
-						<Icon name="facebook" />
-						</List.Item>
-						<List.Item as="a" href="#">
-						<Icon name="twitter" />
-						</List.Item>
-						<List.Item as="a" href="#">
-						<Icon name="instagram" />
-						</List.Item>
-					</List> */}
 					</Container>
 				</Segment>
 			</footer>
